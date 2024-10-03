71bb9dc9e6b2bc2f6327917b378cc9fc912bf4b0
grace <gracemwangi84@gmail.com>
2024-10-03 20:31:44 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Sam'
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value }; // Create a new object with the new key-value pair
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; // Modify the original object
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }; // Clone the employee
+    delete newEmployee[key]; // Delete the key from the clone
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; // Modify the original object
+    return employee;
+  }
+  
